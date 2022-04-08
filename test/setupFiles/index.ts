cd4be7b58e527e83c6d5a6abe1cdb72befbbbb0f
Levix <levix.dev@gmail.com>
2022-04-08 10:26:03 +0800

    feat: add global mock

--- test/setupFiles/index.ts
+++ test/setupFiles/index.ts
@@ -1,5 +1,6 @@
 import { beforeAll, afterAll, afterEach } from "vitest";
 import { server } from "../mocks/server";
+import '../mocks/global';
 
 // 配置服务 onUnhandleRequest: 'error' 只要产生了没有相应类型的请求处理，就会发生错误。
 // 在所有测试之前启动服务器
